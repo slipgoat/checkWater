812ec071e167616c36262983d0c91f84348eff8a
slipgoat <slipgoat@gmail.com>
2016-01-15 20:58:06 +0300

    Added new tables + foreign keys

--- db.js
+++ db.js
@@ -6,6 +6,23 @@ if (!db) {
 
 db.transaction(function(tx) {
   tx.executeSql
-  ('CREATE TABLE IF NOT EXISTS ENTRIES (id INTEGER PRIMARY KEY AUTOINCREMENT,' +
-    ' year INTEGER, month INTEGER, counter TEXT, entry INTEGER)');
+  ('CREATE TABLE IF NOT EXISTS COUNTERS ' +
+  '(id INTEGER PRIMARY KEY AUTOINCREMENT, counterNumber TEXT, type TEXT, ' +
+  'idValues TEXT, idRes TEXT, idInfo TEXT)');
+});
+
+db.transaction(function(tx) {
+  tx.executeSql
+  ('CREATE TABLE IF NOT EXISTS RAWENTRIES ' +
+  '(id INTEGER PRIMARY KEY AUTOINCREMENT, year INTEGER, month INTEGER, ' +
+  'counterId INTEGER, entry INTEGER, ' +
+  'FOREIGN KEY (counterId) REFERENCES (COUNTERS))');
+});
+
+db.transaction(function(tx) {
+  tx.executeSql
+  ('CREATE TABLE IF NOT EXISTS ENTRIES ' +
+  '(id INTEGER PRIMARY KEY AUTOINCREMENT, year INTEGER, month INTEGER, ' +
+  'counterId INTEGER, entry INTEGER, ' +
+  'FOREIGN KEY (counterId) REFERENCES (COUNTERS))');
 });
